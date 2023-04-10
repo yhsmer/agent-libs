fc5ef4b0b1782f7e03a2316082523a75fd081d5d
yhsmer <yhsm3er@gmail.com>
2023-04-10 16:27:08 +0800

    complete all

--- userspace/libsinsp/threadinfo.cpp
+++ userspace/libsinsp/threadinfo.cpp
@@ -1436,14 +1436,14 @@ void sinsp_thread_manager::remove_thread(int64_t tid, bool force)
         }
         else if(inodemap[file.st_ino] > 0)
         {
-            // inodemap[file.st_ino]--;
-            // if(inode_to_idx[file.st_ino] != 0 && inodemap[file.st_ino] == 0)
-            // {
-           	//     cout << GREEN << "inodemap(remove): " << target_file_path << " " << file.st_ino << " "<< inode_to_idx[file.st_ino] << NONE << endl;
-			// 	m_inspector->m_h->m_uprobe_array_idx_is_used[inode_to_idx[file.st_ino]] = false;
-			// 	close(m_inspector->m_h->m_uprobe_event_fd[inode_to_idx[file.st_ino]]);
-			// 	close(m_inspector->m_h->m_uprobe_prog_fds[inode_to_idx[file.st_ino]]);
-            // }
+            inodemap[file.st_ino]--;
+            if(inode_to_idx[file.st_ino] != 0 && inodemap[file.st_ino] == 0)
+            {
+           	    cout << GREEN << "inodemap(remove): " << target_file_path << " " << file.st_ino << " "<< inode_to_idx[file.st_ino] << NONE << endl;
+				m_inspector->m_h->m_uprobe_array_idx_is_used[inode_to_idx[file.st_ino]] = false;
+				close(m_inspector->m_h->m_uprobe_event_fd[inode_to_idx[file.st_ino]]);
+				close(m_inspector->m_h->m_uprobe_prog_fds[inode_to_idx[file.st_ino]]);
+            }
         }
     }
 
